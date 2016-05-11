e1701ae060e9d391a70bd367e95a770b95cb5906
kevin <kevin@localhost.localdomain>
2016-05-11 15:59:19 +0800

    add getlocalip func

--- sysutil.c
+++ sysutil.c
@@ -4,5 +4,11 @@
 int getlocalip(char *ip)
 {
         char host[100]={0};
-        if(gethost)
+        if(gethostname(host,sizeof(host)) <  0)
+                return -1;
+        struct hostent *hp;
+        if((hp = gethostbyname(host)) == NULL)
+                return -1;
+        strcpy(ip,inet_ntoa(*(struct in_addr*)hp->h_addr));
+        return 0;
 }
