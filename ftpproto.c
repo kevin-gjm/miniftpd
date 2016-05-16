8feeed22659523d4564cb111bbaba32c3ed2788a
kevin-gjm <gjm_kevin@163.com>
2016-05-16 08:17:37 +0800

    after compile mapping

--- ftpproto.c
+++ ftpproto.c
@@ -148,7 +148,7 @@ do_pass(sess);
 */
                 int i = 0;
                 int size = sizeof(ctrl_cmds) / sizeof(ctrl_cmds[0]);
-                for(i-0;i<size;i++)
+                for(i=0;i<size;i++)
                 {
                         if(strcmp(ctrl_cmds[i].cmd,sess->cmd) ==0)
                         {
